d0b61daf400067654a3b27f1c727c8a6d40071df
SamanthaSmithleit <98881174+SamanthaSmithleit@users.noreply.github.com>
2022-07-08 11:11:41 +0100

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -3,4 +3,16 @@ import snowflake.connector
 
 streamlit.title('Data Upload Demo')
 
-streamlit.header('Part 1')
+streamlit.header('Pulling Data From Snowflake')
+
+streamlit.header("Fruit load list contains:")
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+      return my_cur.fetchall()
+
+if streamlit.button('Get Fruit Load List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows = get_fruit_load_list()
+   my_cnx.close()
+   streamlit.dataframe(my_data_rows)
